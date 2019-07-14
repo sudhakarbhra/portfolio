ad05d1bdaf751dfc763cb4145e8c31861874d10e
sudhakarbhra <sudhakarbhra@gmail.com>
2019-07-14 11:10:09 +0530

    First Commit

---
+++ assests/main.js
@@ -0,0 +1,8 @@
+    $(window).ready(function() {
+        if(!$('.loader').hasClass('loading')){
+        $('.loader').addClass('loading');
+        $('.loader').css('opacity',0);
+        $('.loader').css('z-index',-1);
+    }
+       
+    });
